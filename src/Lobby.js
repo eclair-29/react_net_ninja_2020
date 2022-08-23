c9692ab1fdf10134f27b08598b48554d0358d1ea
eclair-29 <miguel.dechavez.48@gmail.com>
2022-08-23 11:37:21 +0800

    net ninja react 2020 tutortial #7: Click Events

--- src/Lobby.js
+++ src/Lobby.js
@@ -1,7 +1,16 @@
 const Lobby = () => {
+    const handleClickEvent = (e) => console.log("event object ", e.target.id);
+
     return (
         <div className="content" id="lobby">
             <h3>Lobby Page</h3>
+            {/* click event call without args */}
+            <button id="button_click" onClick={handleClickEvent}>
+                Click Me
+            </button>
+
+            {/* click event call with args */}
+            {/* <button onClick={() => handleClickEvent("Miguel")}>Click Me</button> */}
         </div>
     );
 };
