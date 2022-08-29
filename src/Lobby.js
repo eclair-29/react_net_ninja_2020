aa64da53bae6a72e8488e371ba467bc959de6dc5
eclair-29 <miguel.dechavez.48@gmail.com>
2022-08-30 00:03:03 +0800

    net ninja react 2020 tutortial #15: useEffect Dependencies

--- src/Lobby.js
+++ src/Lobby.js
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { Button } from "antd";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 import { blogList } from "./blogs";
 
 const Lobby = () => {
     const [blogs, setBlogs] = useState(blogList); // data: blogs.csv using useEffect hook
 
+    const [name, setName] = useState("miguel");
+
     const handleDeleteBlog = (blogId) => {
-        const newBlogs = blogs.filter((blog) => blog.id != blogId);
+        const newBlogs = blogs.filter((blog) => blog.id !== blogId);
         setBlogs(newBlogs);
     };
+
+    useEffect(() => {
+        console.log("use effect ran");
+        console.log(name);
+    }, [name]); // dependency array
 
     return (
         <div id="lobby">
@@ -17,6 +25,8 @@ const Lobby = () => {
                 handleDeleteBlog={handleDeleteBlog}
                 header="All Blogs"
             />
+            <Button onClick={() => setName("luigi")}>change name</Button>
+            <p>{name}</p>
         </div>
     );
 };
